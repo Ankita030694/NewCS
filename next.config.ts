51bc607514a09d964d21f921a3a978b68ab04bb4
Bhavy <bhavyajain817@gmail.com>
2025-11-08 10:55:48 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "firebasestorage.googleapis.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "credsettlee.firebasestorage.app",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
       },
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
